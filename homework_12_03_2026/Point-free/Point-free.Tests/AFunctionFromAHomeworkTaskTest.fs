0b45716134cfa8bf900a3f02f0fe4cb243e61275
kirillbenga <kirillbenga@gmail.com>
2026-03-11 16:42:52 +0300

    feat: added point-free homework task

---
+++ homework_12_03_2026/Point-free/Point-free.Tests/AFunctionFromAHomeworkTaskTest.fs
@@ -0,0 +1,13 @@
+﻿module Point_free.Tests
+
+open NUnit.Framework
+open Point_free.AFunctionFromAHomeworkTask
+open FsCheck
+
+let simple x l = List.map (fun y -> y * x) l
+let rec AFunctionFromAHomeworkTaskTest x l =
+    simple x l = aFunctionFromAHomeworkTask x l
+
+[<Test>]
+let ``A function from a homework task test`` () =
+    Check.QuickThrowOnFailure AFunctionFromAHomeworkTaskTest
