47565b55fe5784d3be8338701dc6e22cbbdebf51
kirillbenga <kirillbenga@gmail.com>
2026-03-27 11:09:17 +0300

    refactor and fix: added validation of arguments and fixed a bug in the logic

--- homework_26_03_2026/LocalNetwork/LocalNetwork/Network.fs
+++ homework_26_03_2026/LocalNetwork/LocalNetwork/Network.fs
@@ -1,6 +1,7 @@
 module LocalNetwork.Network
 
 open System
+open System.Collections.Generic
 
 type OC =
     | MacOs
@@ -14,6 +15,8 @@ let private random = Random()
 let private isInfected probability = random.NextDouble() < probability
 
 let private makeAMove infectionProbability (pcArray: Computer[]) (adjacencyMatrix: int[,]) =
+    let infectionSet = HashSet<Computer>()
+
     let rec infectTheNeighbors indexPC (pcArray: Computer[]) (arrayOfNeighbors: int[]) acc =
         if acc >= arrayOfNeighbors.Length then
             ()
@@ -25,14 +28,15 @@ let private makeAMove infectionProbability (pcArray: Computer[]) (adjacencyMatri
                 match pcArray[acc] with
                 | item when item.IsInfected = true -> infectTheNeighbors indexPC pcArray arrayOfNeighbors (acc + 1)
                 | item when item.IsInfected = false ->
-                    item.IsInfected <- item.pcOC |> infectionProbability |> isInfected
+                    infectionSet.Add(item) |> ignore
                     infectTheNeighbors indexPC pcArray arrayOfNeighbors (acc + 1)
                 | _ -> ()
             | _ -> ()
 
     let rec makeAMoveInternal (pcArray: Computer[]) (adjacencyMatrix: int[,]) acc =
         if acc >= adjacencyMatrix.GetLength 0 then
-            ()
+            infectionSet
+            |> Seq.iter (fun x -> x.IsInfected <- x.pcOC |> infectionProbability |> isInfected)
         else
             if pcArray[acc].IsInfected then
                 infectTheNeighbors acc pcArray adjacencyMatrix[acc, *] 0
@@ -42,6 +46,27 @@ let private makeAMove infectionProbability (pcArray: Computer[]) (adjacencyMatri
     makeAMoveInternal pcArray adjacencyMatrix 0
 
 type LocalNetwork(pcArray: Computer[], adjacencyMatrix: int[,], macProb, linuxProb, windowsProb) =
+    do
+        if adjacencyMatrix.GetLength(0) <> adjacencyMatrix.GetLength(1) then
+            invalidArg "adjacencyMatrix" "The matrix must be square."
+
+        if pcArray.Length <> adjacencyMatrix.GetLength(0) then
+            invalidOp "The number of computers does not match the size of the adjacency matrix."
+
+        let hasInvalidValues =
+            adjacencyMatrix |> Seq.cast<int> |> Seq.exists (fun x -> x <> 0 && x <> 1)
+
+        if hasInvalidValues then
+            invalidArg "adjacencyMatrix" "The adjacency matrix should contain only 0 or 1."
+
+        let checkP p name =
+            if p < 0.0 || p > 1.0 then
+                invalidArg name $"The probability of {name} should be in the range [0, 1]."
+
+        checkP macProb "macProb"
+        checkP linuxProb "linuxProb"
+        checkP windowsProb "windowsProb"
+
     let pcArrayCopy =
         pcArray
         |> Array.map (fun c ->
