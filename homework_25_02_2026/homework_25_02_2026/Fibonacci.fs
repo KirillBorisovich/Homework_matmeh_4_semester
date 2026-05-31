c8fbed38405646cf7e581abb0d00ce02aeabca8c
kirillbenga <kirillbenga@gmail.com>
2026-06-01 00:15:22 +0300

    refactor: made changes based on suggestions

--- homework_25_02_2026/homework_25_02_2026/Fibonacci.fs
+++ homework_25_02_2026/homework_25_02_2026/Fibonacci.fs
@@ -1,13 +1,12 @@
 module homework_25_02_2026.Fibonacci
 
 let fibonacci n =
-    let rec fibonacciInternal current preview power index =
-        if index >= power then
-            preview
-        else
-            fibonacciInternal (current + preview) current power (index + 1)
+    let rec fibonacciInternal current previous count =
+        match count with
+        | 1 -> previous
+        | _ -> fibonacciInternal (current + previous) current (count - 1)
 
-    if n = 0 || n < 0 then
+    if n <= 0 then
         None
     else
-        Some(fibonacciInternal 1 1 n 1)
+        Some(fibonacciInternal 1 1 n)
