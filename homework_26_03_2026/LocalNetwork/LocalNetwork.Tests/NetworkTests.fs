35afc127948eae8d2b74febd170b6fbb40e64eb6
kirillbenga <kirillbenga@gmail.com>
2026-06-01 21:56:13 +0300

    refactor: improve LocalNetwork

--- homework_26_03_2026/LocalNetwork/LocalNetwork.Tests/NetworkTests.fs
+++ homework_26_03_2026/LocalNetwork/LocalNetwork.Tests/NetworkTests.fs
@@ -6,233 +6,389 @@ open LocalNetwork.Network
 
 [<Test>]
 let ``Constructor should throw if adjacency matrix is not square`` () =
-    let pcs = [| { pcOC = Windows; IsInfected = false } |]
-    let matrix = array2D [ [ 0; 1 ] ]
+    let computers = [| { OS = Windows; IsInfected = false } |]
+    let matrix = array2D [ [ true; false ] ]
 
-    (fun () -> LocalNetwork(pcs, matrix, 0.5, 0.5, 0.5) |> ignore)
+    (fun () ->
+        LocalNetwork.Create(
+            computers,
+            matrix,
+            { MacProb = 0.5
+              LinuxProb = 0.5
+              WindowsProb = 0.5 }
+        )
+        |> ignore)
     |> should throw typeof<System.ArgumentException>
 
 [<Test>]
 let ``Constructor should throw if pcArray length does not match matrix size`` () =
-    let pcs = [| { pcOC = Windows; IsInfected = false } |]
-    let matrix = array2D [ [ 0; 0 ]; [ 0; 0 ] ]
+    let computers = [| { OS = Windows; IsInfected = false } |]
+    let matrix = array2D [ [ true; false ]; [ false; true ] ]
 
-    (fun () -> LocalNetwork(pcs, matrix, 0.5, 0.5, 0.5) |> ignore)
-    |> should throw typeof<System.InvalidOperationException>
-
-[<Test>]
-let ``Constructor should throw if adjacency matrix contains invalid values`` () =
-    let pcs =
-        [| { pcOC = Windows; IsInfected = false }
-           { pcOC = Linux; IsInfected = false } |]
-
-    let matrix = array2D [ [ 0; 2 ]; [ 2; 0 ] ]
-
-    (fun () -> LocalNetwork(pcs, matrix, 0.5, 0.5, 0.5) |> ignore)
+    (fun () ->
+        LocalNetwork.Create(
+            computers,
+            matrix,
+            { MacProb = 0.5
+              LinuxProb = 0.5
+              WindowsProb = 0.5 }
+        )
+        |> ignore)
     |> should throw typeof<System.ArgumentException>
 
 [<TestCase(-0.1)>]
 [<TestCase(1.1)>]
 let ``Constructor should throw if macProb is out of range`` (prob: float) =
-    let pcs = [| { pcOC = MacOs; IsInfected = false } |]
-    let matrix = array2D [ [ 0 ] ]
+    let computers = [| { OS = MacOs; IsInfected = false } |]
+    let matrix = array2D [ [ false ] ]
 
-    (fun () -> LocalNetwork(pcs, matrix, prob, 0.5, 0.5) |> ignore)
+    (fun () ->
+        LocalNetwork.Create(
+            computers,
+            matrix,
+            { MacProb = prob
+              LinuxProb = 0.5
+              WindowsProb = 0.5 }
+        )
+        |> ignore)
     |> should throw typeof<System.ArgumentException>
 
 [<TestCase(-0.1)>]
 [<TestCase(1.1)>]
 let ``Constructor should throw if linuxProb is out of range`` (prob: float) =
-    let pcs = [| { pcOC = Linux; IsInfected = false } |]
-    let matrix = array2D [ [ 0 ] ]
+    let computers = [| { OS = Linux; IsInfected = false } |]
+    let matrix = array2D [ [ false ] ]
 
-    (fun () -> LocalNetwork(pcs, matrix, 0.5, prob, 0.5) |> ignore)
+    (fun () ->
+        LocalNetwork.Create(
+            computers,
+            matrix,
+            { MacProb = 0.5
+              LinuxProb = prob
+              WindowsProb = 0.5 }
+        )
+        |> ignore)
     |> should throw typeof<System.ArgumentException>
 
 [<TestCase(-0.1)>]
 [<TestCase(1.1)>]
 let ``Constructor should throw if windowsProb is out of range`` (prob: float) =
-    let pcs = [| { pcOC = Windows; IsInfected = false } |]
-    let matrix = array2D [ [ 0 ] ]
+    let computers = [| { OS = Windows; IsInfected = false } |]
+    let matrix = array2D [ [ false ] ]
 
-    (fun () -> LocalNetwork(pcs, matrix, 0.5, 0.5, prob) |> ignore)
+    (fun () ->
+        LocalNetwork.Create(
+            computers,
+            matrix,
+            { MacProb = 0.5
+              LinuxProb = 0.5
+              WindowsProb = prob }
+        )
+        |> ignore)
     |> should throw typeof<System.ArgumentException>
 
 [<Test>]
 let ``No infection should spread if no computer is infected`` () =
-    let pcs =
-        [| { pcOC = Windows; IsInfected = false }
-           { pcOC = Linux; IsInfected = false } |]
+    let computers =
+        [| { OS = Windows; IsInfected = false }; { OS = Linux; IsInfected = false } |]
 
-    let matrix = array2D [ [ 0; 1 ]; [ 1; 0 ] ]
-    let network = LocalNetwork(pcs, matrix, 1.0, 1.0, 1.0)
+    let matrix = array2D [ [ false; true ]; [ true; false ] ]
+
+    let network =
+        LocalNetwork.Create(
+            computers,
+            matrix,
+            { MacProb = 1.0
+              LinuxProb = 1.0
+              WindowsProb = 1.0 }
+        )
+
     network.MakeAMove()
 
-    network.GetComputers()
+    network.Computers
     |> Array.forall (fun c -> not c.IsInfected)
     |> should equal true
 
 [<Test>]
 let ``With probability 1.0 all neighbors of infected computer should become infected`` () =
-    let pcs =
-        [| { pcOC = Windows; IsInfected = true }
-           { pcOC = Windows; IsInfected = false }
-           { pcOC = Windows; IsInfected = false } |]
+    let computers =
+        [| { OS = Windows; IsInfected = true }
+           { OS = Windows; IsInfected = false }
+           { OS = Windows; IsInfected = false } |]
 
-    let matrix = array2D [ [ 0; 1; 1 ]; [ 1; 0; 0 ]; [ 1; 0; 0 ] ]
-    let network = LocalNetwork(pcs, matrix, 1.0, 1.0, 1.0)
+    let matrix =
+        array2D [ [ false; true; true ]; [ true; false; false ]; [ true; false; false ] ]
+
+    let network =
+        LocalNetwork.Create(
+            computers,
+            matrix,
+            { MacProb = 1.0
+              LinuxProb = 1.0
+              WindowsProb = 1.0 }
+        )
+
     network.MakeAMove()
 
-    network.GetComputers()
-    |> Array.forall (fun c -> c.IsInfected)
-    |> should equal true
+    network.Computers |> Array.forall _.IsInfected |> should equal true
 
 [<Test>]
 let ``With probability 0.0 no neighbor should become infected`` () =
-    let pcs =
-        [| { pcOC = Windows; IsInfected = true }
-           { pcOC = Windows; IsInfected = false }
-           { pcOC = Windows; IsInfected = false } |]
+    let computers =
+        [| { OS = Windows; IsInfected = true }
+           { OS = Windows; IsInfected = false }
+           { OS = Windows; IsInfected = false } |]
 
-    let matrix = array2D [ [ 0; 1; 1 ]; [ 1; 0; 0 ]; [ 1; 0; 0 ] ]
-    let network = LocalNetwork(pcs, matrix, 0.0, 0.0, 0.0)
+    let matrix =
+        array2D [ [ false; true; true ]; [ true; false; false ]; [ true; false; false ] ]
+
+    let network =
+        LocalNetwork.Create(
+            computers,
+            matrix,
+            { MacProb = 0.0
+              LinuxProb = 0.0
+              WindowsProb = 0.0 }
+        )
+
     network.MakeAMove()
-    let result = network.GetComputers()
+    let result = network.Computers
     result[0].IsInfected |> should equal true
     result[1].IsInfected |> should equal false
     result[2].IsInfected |> should equal false
 
 [<Test>]
 let ``Disconnected computer should not be infected even with probability 1.0`` () =
-    let pcs =
-        [| { pcOC = Windows; IsInfected = true }
-           { pcOC = Windows; IsInfected = false } |]
+    let computers =
+        [| { OS = Windows; IsInfected = true }; { OS = Windows; IsInfected = false } |]
 
-    let matrix = array2D [ [ 0; 0 ]; [ 0; 0 ] ]
-    let network = LocalNetwork(pcs, matrix, 1.0, 1.0, 1.0)
+    let matrix = array2D [ [ false; false ]; [ false; false ] ]
+
+    let network =
+        LocalNetwork.Create(
+            computers,
+            matrix,
+            { MacProb = 1.0
+              LinuxProb = 1.0
+              WindowsProb = 1.0 }
+        )
+
     network.MakeAMove()
-    let result = network.GetComputers()
+    let result = network.Computers
     result[0].IsInfected |> should equal true
     result[1].IsInfected |> should equal false
 
 [<Test>]
 let ``Already infected computer should stay infected`` () =
-    let pcs = [| { pcOC = Windows; IsInfected = true } |]
-    let matrix = array2D [ [ 0 ] ]
-    let network = LocalNetwork(pcs, matrix, 0.0, 0.0, 0.0)
+    let computers = [| { OS = Windows; IsInfected = true } |]
+    let matrix = array2D [ [ false ] ]
+
+    let network =
+        LocalNetwork.Create(
+            computers,
+            matrix,
+            { MacProb = 0.0
+              LinuxProb = 0.0
+              WindowsProb = 0.0 }
+        )
+
     network.MakeAMove()
-    (network.GetComputers()[0]).IsInfected |> should equal true
+    (network.Computers[0]).IsInfected |> should equal true
 
 [<Test>]
 let ``Infection should propagate through a chain one step at a time`` () =
-    let pcs =
-        [| { pcOC = Windows; IsInfected = true }
-           { pcOC = Windows; IsInfected = false }
-           { pcOC = Windows; IsInfected = false } |]
+    let computers =
+        [| { OS = Windows; IsInfected = true }
+           { OS = Windows; IsInfected = false }
+           { OS = Windows; IsInfected = false } |]
 
-    let matrix = array2D [ [ 0; 1; 0 ]; [ 1; 0; 1 ]; [ 0; 1; 0 ] ]
-    let network = LocalNetwork(pcs, matrix, 1.0, 1.0, 1.0)
+    let matrix =
+        array2D [ [ false; true; false ]; [ true; false; true ]; [ false; true; false ] ]
+
+    let network =
+        LocalNetwork.Create(
+            computers,
+            matrix,
+            { MacProb = 1.0
+              LinuxProb = 1.0
+              WindowsProb = 1.0 }
+        )
 
     network.MakeAMove()
-    let after1 = network.GetComputers()
+    let after1 = network.Computers
     after1[0].IsInfected |> should equal true
     after1[1].IsInfected |> should equal true
     after1[2].IsInfected |> should equal false
 
     network.MakeAMove()
-    let after2 = network.GetComputers()
+    let after2 = network.Computers
     after2[2].IsInfected |> should equal true
 
 [<Test>]
 let ``Only Windows neighbor should be infected when only windowsProb is 1.0`` () =
-    let pcs =
-        [| { pcOC = Linux; IsInfected = true }
-           { pcOC = Windows; IsInfected = false }
-           { pcOC = Linux; IsInfected = false }
-           { pcOC = MacOs; IsInfected = false } |]
+    let computers =
+        [| { OS = Linux; IsInfected = true }
+           { OS = Windows; IsInfected = false }
+           { OS = Linux; IsInfected = false }
+           { OS = MacOs; IsInfected = false } |]
 
     let matrix =
-        array2D [ [ 0; 1; 1; 1 ]; [ 1; 0; 0; 0 ]; [ 1; 0; 0; 0 ]; [ 1; 0; 0; 0 ] ]
+        array2D
+            [ [ false; true; true; true ]
+              [ true; false; false; false ]
+              [ true; false; false; false ]
+              [ true; false; false; false ] ]
 
-    let network = LocalNetwork(pcs, matrix, 0.0, 0.0, 1.0)
+    let network =
+        LocalNetwork.Create(
+            computers,
+            matrix,
+            { MacProb = 0.0
+              LinuxProb = 0.0
+              WindowsProb = 1.0 }
+        )
+
     network.MakeAMove()
-    let result = network.GetComputers()
+    let result = network.Computers
     result[1].IsInfected |> should equal true
     result[2].IsInfected |> should equal false
     result[3].IsInfected |> should equal false
 
 [<Test>]
 let ``Only Linux neighbor should be infected when only linuxProb is 1.0`` () =
-    let pcs =
-        [| { pcOC = Windows; IsInfected = true }
-           { pcOC = Linux; IsInfected = false }
-           { pcOC = Windows; IsInfected = false }
-           { pcOC = MacOs; IsInfected = false } |]
+    let computers =
+        [| { OS = Windows; IsInfected = true }
+           { OS = Linux; IsInfected = false }
+           { OS = Windows; IsInfected = false }
+           { OS = MacOs; IsInfected = false } |]
 
     let matrix =
-        array2D [ [ 0; 1; 1; 1 ]; [ 1; 0; 0; 0 ]; [ 1; 0; 0; 0 ]; [ 1; 0; 0; 0 ] ]
+        array2D
+            [ [ false; true; true; true ]
+              [ true; false; false; false ]
+              [ true; false; false; false ]
+              [ true; false; false; false ] ]
 
-    let network = LocalNetwork(pcs, matrix, 0.0, 1.0, 0.0)
+    let network =
+        LocalNetwork.Create(
+            computers,
+            matrix,
+            { MacProb = 0.0
+              LinuxProb = 1.0
+              WindowsProb = 0.0 }
+        )
+
     network.MakeAMove()
-    let result = network.GetComputers()
+    let result = network.Computers
     result[1].IsInfected |> should equal true
     result[2].IsInfected |> should equal false
     result[3].IsInfected |> should equal false
 
 [<Test>]
 let ``Only MacOs neighbor should be infected when only macProb is 1.0`` () =
-    let pcs =
-        [| { pcOC = Windows; IsInfected = true }
-           { pcOC = MacOs; IsInfected = false }
-           { pcOC = Windows; IsInfected = false }
-           { pcOC = Linux; IsInfected = false } |]
+    let computers =
+        [| { OS = Windows; IsInfected = true }
+           { OS = MacOs; IsInfected = false }
+           { OS = Windows; IsInfected = false }
+           { OS = Linux; IsInfected = false } |]
 
     let matrix =
-        array2D [ [ 0; 1; 1; 1 ]; [ 1; 0; 0; 0 ]; [ 1; 0; 0; 0 ]; [ 1; 0; 0; 0 ] ]
+        array2D
+            [ [ false; true; true; true ]
+              [ true; false; false; false ]
+              [ true; false; false; false ]
+              [ true; false; false; false ] ]
 
-    let network = LocalNetwork(pcs, matrix, 1.0, 0.0, 0.0)
+    let network =
+        LocalNetwork.Create(
+            computers,
+            matrix,
+            { MacProb = 1.0
+              LinuxProb = 0.0
+              WindowsProb = 0.0 }
+        )
+
     network.MakeAMove()
-    let result = network.GetComputers()
+    let result = network.Computers
     result[1].IsInfected |> should equal true
     result[2].IsInfected |> should equal false
     result[3].IsInfected |> should equal false
 
 [<Test>]
 let ``Modifying original array after construction should not affect the network`` () =
-    let pcs = [| { pcOC = Windows; IsInfected = false } |]
-    let matrix = array2D [ [ 0 ] ]
-    let network = LocalNetwork(pcs, matrix, 0.5, 0.5, 0.5)
-    pcs[0].IsInfected <- true
-    (network.GetComputers()[0]).IsInfected |> should equal false
+    let computers = [| { OS = Windows; IsInfected = false } |]
+    let matrix = array2D [ [ false ] ]
+
+    let network =
+        LocalNetwork.Create(
+            computers,
+            matrix,
+            { MacProb = 0.5
+              LinuxProb = 0.5
+              WindowsProb = 0.5 }
+        )
+
+    computers[0].IsInfected <- true
+    (network.Computers[0]).IsInfected |> should equal false
 
 [<Test>]
 let ``GetComputers should return a defensive copy`` () =
-    let pcs = [| { pcOC = Windows; IsInfected = false } |]
-    let matrix = array2D [ [ 0 ] ]
-    let network = LocalNetwork(pcs, matrix, 0.5, 0.5, 0.5)
-    let copy = network.GetComputers()
+    let computers = [| { OS = Windows; IsInfected = false } |]
+    let matrix = array2D [ [ false ] ]
+
+    let network =
+        LocalNetwork.Create(
+            computers,
+            matrix,
+            { MacProb = 0.5
+              LinuxProb = 0.5
+              WindowsProb = 0.5 }
+        )
+
+    let copy = network.Computers
     copy[0].IsInfected <- true
-    (network.GetComputers()[0]).IsInfected |> should equal false
+    (network.Computers[0]).IsInfected |> should equal false
 
 [<Test>]
 let ``Fully connected network with probability 1.0 should infect all in one move`` () =
-    let pcs =
-        [| { pcOC = Linux; IsInfected = true }
-           { pcOC = Linux; IsInfected = false }
-           { pcOC = Linux; IsInfected = false }
-           { pcOC = Linux; IsInfected = false } |]
+    let computers =
+        [| { OS = Linux; IsInfected = true }
+           { OS = Linux; IsInfected = false }
+           { OS = Linux; IsInfected = false }
+           { OS = Linux; IsInfected = false } |]
 
     let matrix =
-        array2D [ [ 0; 1; 1; 1 ]; [ 1; 0; 1; 1 ]; [ 1; 1; 0; 1 ]; [ 1; 1; 1; 0 ] ]
+        array2D
+            [ [ false; true; true; true ]
+              [ true; false; true; true ]
+              [ true; true; false; true ]
+              [ true; true; true; false ] ]
 
-    let network = LocalNetwork(pcs, matrix, 1.0, 1.0, 1.0)
+    let network =
+        LocalNetwork.Create(
+            computers,
+            matrix,
+            { MacProb = 1.0
+              LinuxProb = 1.0
+              WindowsProb = 1.0 }
+        )
+
     network.MakeAMove()
-    network.GetComputers() |> Array.forall _.IsInfected |> should equal true
+    network.Computers |> Array.forall _.IsInfected |> should equal true
 
 [<Test>]
 let ``Single uninfected computer should stay uninfected`` () =
-    let pcs = [| { pcOC = MacOs; IsInfected = false } |]
-    let matrix = array2D [ [ 0 ] ]
-    let network = LocalNetwork(pcs, matrix, 1.0, 1.0, 1.0)
+    let computers = [| { OS = MacOs; IsInfected = false } |]
+    let matrix = array2D [ [ false ] ]
+
+    let network =
+        LocalNetwork.Create(
+            computers,
+            matrix,
+            { MacProb = 1.0
+              LinuxProb = 1.0
+              WindowsProb = 1.0 }
+        )
+
     network.MakeAMove()
-    (network.GetComputers()[0]).IsInfected |> should equal false
+    (network.Computers[0]).IsInfected |> should equal false
--- homework_26_03_2026/LocalNetwork/LocalNetwork/Network.fs
+++ homework_26_03_2026/LocalNetwork/LocalNetwork/Network.fs
@@ -3,91 +3,87 @@ module LocalNetwork.Network
 open System
 open System.Collections.Generic
 
-type OC =
+type OS =
     | MacOs
     | Linux
     | Windows
 
-type Computer = { pcOC: OC; mutable IsInfected: bool }
+type Computer = { OS: OS; mutable IsInfected: bool }
 
-let private random = Random()
+type InfectionProbabilities =
+    { MacProb: float
+      LinuxProb: float
+      WindowsProb: float }
 
-let private isInfected probability = random.NextDouble() < probability
+    member this.ForOS(os: OS) =
+        match os with
+        | MacOs -> this.MacProb
+        | Linux -> this.LinuxProb
+        | Windows -> this.WindowsProb
 
-let private makeAMove infectionProbability (pcArray: Computer[]) (adjacencyMatrix: int[,]) =
-    let infectionSet = HashSet<int * Computer>()
+type LocalNetwork private (computers: Computer[], adjacencyMatrix: bool[,], probabilities: InfectionProbabilities) =
 
-    let rec infectTheNeighbors indexPC (pcArray: Computer[]) (arrayOfNeighbors: int[]) acc =
-        if acc >= arrayOfNeighbors.Length then
-            ()
+    let random = Random(Environment.TickCount)
+
+    let mutable currentComputers =
+        computers |> Array.map (fun c -> { c with IsInfected = c.IsInfected })
+
+    let size = computers.Length
+
+    static member Create(computers: Computer[], adjacencyMatrix: bool[,], probabilities: InfectionProbabilities) =
+        if
+            adjacencyMatrix.GetLength(0) <> computers.Length
+            || adjacencyMatrix.GetLength(1) <> computers.Length
+        then
+            invalidArg "adjacencyMatrix" "Matrix dimensions must match number of computers."
+
+        if
+            probabilities.MacProb < 0.0
+            || probabilities.MacProb > 1.0
+            || probabilities.LinuxProb < 0.0
+            || probabilities.LinuxProb > 1.0
+            || probabilities.WindowsProb < 0.0
+            || probabilities.WindowsProb > 1.0
+        then
+            invalidArg "probabilities" "All probabilities must be between 0.0 and 1.0."
+
+        LocalNetwork(computers, adjacencyMatrix, probabilities)
+
+    member _.Computers =
+        currentComputers |> Array.map (fun c -> { c with IsInfected = c.IsInfected })
+
+    member private _.TryInfect(index: int) : bool =
+        let computer = currentComputers[index]
+
+        if not computer.IsInfected then
+            let prob = probabilities.ForOS(computer.OS)
+            random.NextDouble() < prob
         else
-            match arrayOfNeighbors[acc] with
-            | 0 -> infectTheNeighbors indexPC pcArray arrayOfNeighbors (acc + 1)
-            | 1 when acc = indexPC -> infectTheNeighbors indexPC pcArray arrayOfNeighbors (acc + 1)
-            | 1 ->
-                match pcArray[acc] with
-                | item when item.IsInfected = true -> infectTheNeighbors indexPC pcArray arrayOfNeighbors (acc + 1)
-                | item when item.IsInfected = false ->
-                    infectionSet.Add((acc, item)) |> ignore
-                    infectTheNeighbors indexPC pcArray arrayOfNeighbors (acc + 1)
-                | _ -> ()
-            | _ -> ()
+            false
 
-    let rec makeAMoveInternal (pcArray: Computer[]) (adjacencyMatrix: int[,]) acc =
-        if acc >= adjacencyMatrix.GetLength 0 then
-            infectionSet
-            |> Seq.iter (fun item ->
-                let pc = snd item
-                pc.IsInfected <- pc.pcOC |> infectionProbability |> isInfected)
-        else
-            if pcArray[acc].IsInfected then
-                infectTheNeighbors acc pcArray adjacencyMatrix[acc, *] 0
+    member private this.Step() : bool =
+        let newlyInfected = HashSet<int>()
 
-            makeAMoveInternal pcArray adjacencyMatrix (acc + 1)
+        for i in 0 .. size - 1 do
+            if currentComputers[i].IsInfected then
+                for j in 0 .. size - 1 do
+                    if adjacencyMatrix[i, j] && not currentComputers[j].IsInfected then
+                        if this.TryInfect(j) then
+                            newlyInfected.Add(j) |> ignore
 
-    makeAMoveInternal pcArray adjacencyMatrix 0
+        for idx in newlyInfected do
+            currentComputers[idx].IsInfected <- true
 
-type LocalNetwork(pcArray: Computer[], adjacencyMatrix: int[,], macProb, linuxProb, windowsProb) =
-    do
-        if adjacencyMatrix.GetLength(0) <> adjacencyMatrix.GetLength(1) then
-            invalidArg "adjacencyMatrix" "The matrix must be square."
+        newlyInfected.Count > 0
 
-        if pcArray.Length <> adjacencyMatrix.GetLength(0) then
-            invalidOp "The number of computers does not match the size of the adjacency matrix."
+    member this.RunSimulation(printState: Computer[] -> unit) =
+        printState this.Computers
+        let mutable changed = true
 
-        let hasInvalidValues =
-            adjacencyMatrix |> Seq.cast<int> |> Seq.exists (fun x -> x <> 0 && x <> 1)
+        while changed do
+            changed <- this.Step()
 
-        if hasInvalidValues then
-            invalidArg "adjacencyMatrix" "The adjacency matrix should contain only 0 or 1."
+            if changed then
+                printState this.Computers
 
-        let checkP p name =
-            if p < 0.0 || p > 1.0 then
-                invalidArg name $"The probability of {name} should be in the range [0, 1]."
-
-        checkP macProb "macProb"
-        checkP linuxProb "linuxProb"
-        checkP windowsProb "windowsProb"
-
-    let pcArrayCopy =
-        pcArray
-        |> Array.map (fun c ->
-            { pcOC = c.pcOC
-              IsInfected = c.IsInfected })
-
-    let adjacencyMatrixCopy = Array2D.copy adjacencyMatrix
-
-    let infectionProbability =
-        function
-        | MacOs -> macProb
-        | Linux -> linuxProb
-        | Windows -> windowsProb
-
-    member this.MakeAMove() =
-        makeAMove infectionProbability pcArrayCopy adjacencyMatrixCopy
-
-    member this.GetComputers() =
-        pcArrayCopy
-        |> Array.map (fun c ->
-            { pcOC = c.pcOC
-              IsInfected = c.IsInfected })
+    member this.MakeAMove() = this.Step() |> ignore
